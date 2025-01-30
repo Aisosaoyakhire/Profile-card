5edcbdfc8a55785dbba6aa8ca02e9e7e08395efd
Oyakhire Aisosa Margaret <aisosagraphicdesign22@gmsil.come>
2025-01-30 12:39:38 +0100

    modified bio

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ function updateTime() {
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+function toggleReadMore() {
+    const moreText = document.getElementById('moreText');
+    const readMoreBtn = document.getElementById('readMoreBtn');
+
+    if (moreText.style.display === 'none') {
+        moreText.style.display = 'inline';
+        readMoreBtn.textContent = 'Read Less';
+    } else {
+        moreText.style.display = 'none';
+        readMoreBtn.textContent = 'Read More';
+    }
+}
